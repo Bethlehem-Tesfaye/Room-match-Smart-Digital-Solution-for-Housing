16c3a6b598910dc354eedd25a8c7627a3277341b
Bethlehem-Tesfaye <bethlehem.tesfaye15@gmail.com>
2026-02-19 02:40:41 +0400

    fix: Update trusted origin from localhost:3000 to localhost:8000

--- server/src/models/auth/auth.js
+++ server/src/models/auth/auth.js
@@ -23,7 +23,7 @@ export const auth = betterAuth({
   trustedOrigins: [
     env.CLIENT_URL,
     "http://localhost:5173",
-    "http://localhost:3000",
+    "http://localhost:8000",
     "http://localhost:4000"
   ].filter(Boolean),
 
