da67bba7de4626fbcefac8835e131fab33bee27e
Jubalme <bethelasnake0000@gmail.com>
2026-05-15 09:38:32 +0300

    fix prettier

--- server/src/models/auth/auth.js
+++ server/src/models/auth/auth.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { betterAuth, logger } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { createAuthMiddleware } from "better-auth/api";
